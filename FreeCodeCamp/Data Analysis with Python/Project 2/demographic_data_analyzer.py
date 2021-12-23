a05126706381f2547cacc6103c3c1b603b6219d4
Jamie <jamie@JamieDeb.mynet>
2021-12-23 10:35:13 +0000

    Partial completion of Project 2

--- FreeCodeCamp/Data Analysis with Python/Project 2/demographic_data_analyzer.py
+++ FreeCodeCamp/Data Analysis with Python/Project 2/demographic_data_analyzer.py
@@ -1,25 +1,37 @@
 import pandas as pd
-
-
+print('Hello Bitches!')
 def calculate_demographic_data(print_data=True):
-    # Read data from file
-    df = None
+     # Read data from file
+    df = pd.read_csv("adult.data.csv")
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df.value_counts('race')
 
     # What is the average age of men?
-    average_age_men = None
+    age_series = []
+    average_age_men = round(df.mean('age' == 'Male')['age'],1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    number_of_bachelors = df['education'].value_counts().Bachelors
+    number_of_records = df.shape[0]
+
+    percentage_bachelors = round(number_of_bachelors/number_of_records*100,1)
+
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
-    # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    # Experiments
+    number_of_high_earners = df['salary'].value_counts()['>50K']
+    number_of_higher_education = df['education'].value_counts()['Doctorate'] + df['education'].value_counts()['Bachelors'] + df['education'].value_counts()['Masters']
+
+    high_ed_and_high_earners = df[(df['salary'] == '>50K') & ((df['education'] == 'Doctorate') | (df['education'] == 'Masters') | (df['education'] == 'Bachelors'))]
+
+
+    low_ed_and_high_earners = df[(df['salary'] == '>50K') & ((df['education'] != 'Doctorate') | (df['education'] != 'Masters') | (df['education'] != 'Bachelors'))]
+
+    higher_education = round(high_ed_and_high_earners.shape[0]/number_of_records*100, 1)
+    lower_education = round(low_ed_and_high_earners.shape[0]/number_of_records*100,1)
 
     # percentage with salary >50K
     higher_education_rich = None
@@ -43,7 +55,7 @@ def calculate_demographic_data(print_data=True):
     # DO NOT MODIFY BELOW THIS LINE
 
     if print_data:
-        print("Number of each race:\n", race_count) 
+        print("Number of each race:\n", race_count)
         print("Average age of men:", average_age_men)
         print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
         print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
